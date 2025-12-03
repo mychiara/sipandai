f8df31c71ddfa4935390de4b3fce9f49e0e00a9b
Mas Andy <85479169+mychiara@users.noreply.github.com>
2025-12-03 23:54:39 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+  // Konfigurasi Firebase
+  firebase: {
+    apiKey: "AIzaSyBrUof5Nc8mgvBov1qvWM363RBUTBzW15o",
+    authDomain: "sipandai-bc46f.firebaseapp.com",
+    projectId: "sipandai-bc46f",
+    storageBucket: "sipandai-bc46f.firebasestorage.app",
+    messagingSenderId: "962522187009",
+    appId: "1:962522187009:web:d1ea2e7ce9a9e9a22c8700",
+    measurementId: "G-CTZ38C68LS"
+  },
+  
+  // Konfigurasi Supabase
+  supabase: {
+    url: 'https://oklwromoxuqacuwqqeao.supabase.co',
+    key: 'sb_publishable_WRJ0OpuXZzs-KM_eP3Gbxw_DGMDP5Lu'
+  }
+};
